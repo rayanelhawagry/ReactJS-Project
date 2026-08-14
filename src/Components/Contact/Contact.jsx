eeccde73509b0adfe9bdbec25f5345054c93344b
Rayan Elhawagry <se.mycareer@gmail.com>
2026-08-14 16:02:13 +0300

    build and style Contact component

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import '../Contact/Contact.modules.css'
 
 export default function Contact() {
     return (
         <>
-            <h1>Contact</h1>
+            <div className="contact d-flex justify-content-center align-items-start">
+                <div className="content d-flex flex-column justify-content-center align-items-center mb-5 w-100">
+                    <div className="title">
+                        <h2 className='text-uppercase fw-bolder mt-4 mb-4 text-center'>contact component</h2>
+                        <div className="starLine d-flex justify-content-center align-items-center mb-3">
+                            <div className="line me-3"></div>
+                            <i className='star fa-solid fa-star'></i>
+                            <div className="line ms-3"></div>
+                        </div>
+                    </div>
+
+                    <div className="form mt-5 w-own">
+                        <div className='container d-flex justify-content-center align-items-center'>
+                            <form className='w-100'>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleFormControlInput1" className="form-label fw-medium">Username</label>
+                                    <input type="text" className="form-control" id="exampleFormControlInput1" autoComplete='off' />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleFormControlInput2" className="form-label fw-medium">Age</label>
+                                    <input type="number" className="form-control" id="exampleFormControlInput2" autoComplete='off' />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleFormControlInput3" className="form-label fw-medium">Email Address</label>
+                                    <input type="email" className="form-control" id="exampleFormControlInput3" autoComplete='off' />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="exampleFormControlInput4" className="form-label fw-medium">Password</label>
+                                    <input type="password" className="form-control" id="exampleFormControlInput4" autoComplete='off' />
+                                </div>
+                                <div className="btn d-flex justify-content-center align-items-center">
+                                    <button type="button" className="btn btn-warning">Send Message</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div >
+            </div >
         </>
     )
 }
