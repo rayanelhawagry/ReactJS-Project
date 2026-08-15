647cb5cfec2e809706a4c7b1357894cf767ffd83
Rayan Elhawagry <se.mycareer@gmail.com>
2026-08-15 16:18:37 +0300

    Edit nav-link's width and add margin-top for better style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,14 +6,16 @@ export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-0">
-                <div className="container pt-3 pb-3 d-flex justify-content-between">
-                    <div>
+                <div className="container pt-3 pb-3 d-flex justify-content-between align-items-center">
+                    <div className='title'>
                         <NavLink className="navbar-brand text-light fs-2 fw-bolder text-uppercase" to={''}>Start Framework</NavLink>
                     </div>
-                    <div>
+                    <div className='btn'>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon" />
                         </button>
+                    </div>
+                    <div className='list'>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item ms-2">
