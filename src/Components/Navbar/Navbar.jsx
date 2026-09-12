a907a970dcd52e9a562884061e322ce89bd1fbd6
Rayan Elhawagry <se.mycareer@gmail.com>
2026-09-12 05:57:21 +0300

    Replace NavLink with Link in Navbar title

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.modules.css'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     return (
@@ -8,7 +8,7 @@ export default function Navbar() {
             <nav className="navbar navbar-expand-lg position-sticky top-0 start-0 end-0">
                 <div className="container pt-3 pb-3 d-flex justify-content-between align-items-center">
                     <div className='title'>
-                        <NavLink className="navbar-brand text-light fs-2 fw-bolder text-uppercase" to={''}>Start Framework</NavLink>
+                        <Link className="navbar-brand text-light fs-2 fw-bolder text-uppercase" to={''}>Start Framework</Link>
                     </div>
                     <div className='btn'>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
