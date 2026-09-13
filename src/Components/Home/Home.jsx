b496239aac49c242e960e3d9624b3dbf0d7b1086
Rayan Elhawagry <se.mycareer@gmail.com>
2026-09-13 06:27:33 +0300

    Refactor Home component & clean up CSS

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import avatar from '../../assets/avatar.svg'
 import '../Home/Home.modules.css'
 
-export default function Home({ currentTitle, setCurrentTitle }) {
+export default function Home({ setCurrentTitle }) {
     useEffect(() => {
         setCurrentTitle('Home')
     }, [])
@@ -15,11 +15,11 @@ export default function Home({ currentTitle, setCurrentTitle }) {
                     <div className="avatar">
                         <img src={avatar} alt="Avatar" />
                     </div>
-                    <h2 className='text-uppercase text-light fw-bolder mt-5 mb-3'>Start Framework</h2>
-                    <div className="starLine d-flex justify-content-center align-items-center mb-3">
-                        <div className="line me-3"></div>
+                    <h1 className='text-uppercase text-light fw-bolder mt-5 mb-3'>Start Framework</h1>
+                    <div className="starLine d-flex justify-content-center align-items-center gap-3 mb-3">
+                        <div className="line"></div>
                         <i className='fa-solid fa-star text-light'></i>
-                        <div className="line ms-3"></div>
+                        <div className="line"></div>
                     </div>
                     <p className='text-light'>Graphic Artist - Web Designer - Illustrator</p>
                 </div>
