1d78608058f0c5559f77a1f8fca684682772b979
Rayan Elhawagry <se.mycareer@gmail.com>
2026-09-13 07:01:42 +0300

    Refactor About component: remove unused prop & improve layout

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import '../About/About.modules.css'
 
-export default function About({ currentTitle, setCurrentTitle }) {
+export default function About({ setCurrentTitle }) {
     useEffect(() => {
         setCurrentTitle('About')
     }, [])
@@ -12,16 +12,16 @@ export default function About({ currentTitle, setCurrentTitle }) {
             <div className="about d-flex justify-content-center align-items-center">
                 <div className="content d-flex flex-column justify-content-center align-items-center">
                     <div className="title">
-                        <h2 className='text-uppercase text-light fw-bolder mt-5 mb-3'>about component</h2>
-                        <div className="starLine d-flex justify-content-center align-items-center mb-3">
-                            <div className="line me-3"></div>
+                        <h1 className='text-uppercase text-light fw-bolder mt-5 mb-3'>about component</h1>
+                        <div className="starLine d-flex justify-content-center align-items-center gap-3 mb-3">
+                            <div className="line"></div>
                             <i className='fa-solid fa-star text-light'></i>
-                            <div className="line ms-3"></div>
+                            <div className="line"></div>
                         </div>
                     </div>
                     <div className="details">
-                        <div className="container d-md-flex justify-content-between">
-                            <p className='text-light pe-4'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+                        <div className="container d-md-flex justify-content-between gap-4">
+                            <p className='text-light'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
                             <p className='text-light'>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
                         </div>
                     </div>
