2af1e11bd521e4e8a7d003dcde82dff59c54017b
Rayan Elhawagry <se.mycareer@gmail.com>
2026-09-14 08:17:48 +0300

    Portfolio component: add image selection functionality & improve card layout

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Portfolio/Portfolio.modules.css'
 import img1 from '../../assets/img1.png'
 import img2 from '../../assets/img2.png'
@@ -6,6 +6,7 @@ import img3 from '../../assets/img3.png'
 
 export default function Portfolio({ setCurrentTitle }) {
     const imgs = [img1, img2, img3, img1, img2, img3]
+    const [selectedImg, setSelectedImg] = useState(null)
 
     useEffect(() => {
         setCurrentTitle('Portfolio')
@@ -28,15 +29,17 @@ export default function Portfolio({ setCurrentTitle }) {
                     <div className="details">
                         <div className="container d-flex flex-wrap justify-content-center align-items-center gap-5">
                             {imgs.map((img, index) =>
-                                <div className="outer-card w-own" key={index}>
-                                    <div className="inner-card cursor-pointer position-relative">
-                                        <img src={img} alt="img" className='w-100 rounded-3' />
-                                        <div className="layout rounded-3 position-absolute top-0 bottom-0 start-0 end-0 bg-layout d-flex justify-content-center align-items-center">
-                                            <i className="fa-solid fa-plus fa-6x text-white"></i>
-                                        </div>
+                                <div className="card w-own cursor-pointer position-relative rounded-3" key={index} onClick={() => setSelectedImg(img)}>
+                                    <img src={img} alt="Img" className='w-100 rounded-3' />
+                                    <div className="layout rounded-3 position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center">
+                                        <i className="fa-solid fa-plus fa-6x text-white"></i>
                                     </div>
                                 </div>)}
                         </div>
+                        {selectedImg &&
+                            <div className='selectedImgLayout position-fixed top-0 start-0 end-0 bottom-0 d-flex justify-content-center align-items-center' onClick={() => setSelectedImg(null)}>
+                                <img src={selectedImg} alt="Img" />
+                            </div>}
                     </div>
                 </div>
             </div>
